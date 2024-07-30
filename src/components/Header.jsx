486a65fe2a799413d2130fb93b63ecd7d2df9b29
Niko7even <pozzernico@gmail.com>
2024-07-30 00:05:56 -0300

    Testeando componentes de React

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+//import { Fragment } from "react" // Sirve para no repetir muchos div
+
+export default function Header() {
+
+    const total = 100
+
+    //El return debe estar siempre, y es el que se va a mostrar en pantalla
+    return(
+        <>
+        <p>El total a pagar es: {total}</p>
+        <p>El total a pagar es: {total}</p>
+        <p>El total a pagar es: {total}</p>
+        </>
+    )
+}
